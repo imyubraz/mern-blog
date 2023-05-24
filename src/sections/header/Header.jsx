00fdba8156b0939f4a6a243e4794954f1bdba316
Yubraj <iyubrajpoudel@gmail.com>
2023-05-24 15:46:01 +0545

    Header styling fixes.

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -41,24 +41,24 @@ const Menu = ({menuitem}) =>{
         {
             menuitem.type === "link"
             && 
-            <li className='menu hover:font-bold w-full md:w-auto hover:text-yellow-500 md:hover:text-primary transition-all ease-out duration-200'><a href={menuitem.path}>{menuitem.name}</a></li>
+            <li className='menu hover:font-bold w-full lg:w-auto hover:text-yellow-500 lg:hover:text-primary transition-all ease-out duration-200'><a href={menuitem.path}>{menuitem.name}</a></li>
         }
         {
             menuitem.type === "dropdown"
             &&
             <>
-                <li className='menu hover:font-bold w-full md:w-auto transition-all ease-out duration-200 relative group cursor-pointer' onClick={()=>setShowDropdown((prevState)=>!prevState)}>
-                    <div className='flex gap-1 items-center group-hover:text-yellow-500 md:group-hover:text-primary'>
+                <li className='menu hover:font-bold w-full lg:w-auto transition-all ease-out duration-200 relative group cursor-pointer' onClick={()=>setShowDropdown((prevState)=>!prevState)}>
+                    <div className='flex gap-1 items-center group-hover:text-yellow-500 lg:group-hover:text-primary'>
                         {menuitem.name}
                         <IoIosArrowDown/>
                     </div>
                     {
                         showDropdown &&
                         (
-                            <div className="dropdown-items static md:absolute left-1 md:min-w-[8rem] bg-transparent md:bg-cyan-50 p-2 mt-4 flex flex-col gap-4 shadow-sm">
+                            <div className="dropdown-items static lg:absolute left-1 lg:min-w-[8rem] bg-transparent lg:bg-cyan-50 p-2 mt-4 flex flex-col gap-4 shadow-sm">
                                 {
                                 menuitem.dropdownitems.map((item)=>
-                                    <li className='menu hover:font-bold w-full md:w-auto hover:text-yellow-500 md:hover:text-primary transition-all ease-out duration-200'><a href={item.path}>{item.name}</a></li>
+                                    <li className='menu hover:font-bold w-full lg:w-auto hover:text-yellow-500 lg:hover:text-primary transition-all ease-out duration-200'><a href={item.path}>{item.name}</a></li>
                                 )
                                 }
                             </div>
@@ -80,7 +80,7 @@ const [showMobileMenu, setShowMobileMenu ] = useState(false);
 
   return (
     <>
-    <div className={`wrapper bg-white shadow-md md:shadow-none w-full`}>
+    <div className={`wrapper bg-white shadow-lg lg:shadow-none w-full`}>
         <div className={`container p-2 h-[80px] lg:min-h-[10vh] relative lg:static flex`}>
             <div className="row w-full flex gap-8 items-center">
                 <div className="col w-1/4">
@@ -90,18 +90,18 @@ const [showMobileMenu, setShowMobileMenu ] = useState(false);
                 </div>
                 <div className="col grow flex justify-end items-center">
                     {!showMobileMenu?
-                    <div className="md:hidden menu-icon text-3xl cursor-pointer" onClick={()=>setShowMobileMenu(true)}>
+                    <div className="lg:hidden menu-icon text-3xl cursor-pointer" onClick={()=>setShowMobileMenu(true)}>
                         <AiOutlineMenu/>
                     </div>
                     :
-                    <div className="md:hidden close-icon text-3xl cursor-pointer" onClick={()=>setShowMobileMenu(false)}>
+                    <div className="lg:hidden close-icon text-3xl cursor-pointer" onClick={()=>setShowMobileMenu(false)}>
                         <AiOutlineClose />
                     </div>
                     }
-                    <div className={`nav-wrapper absolute top-[80px] ${showMobileMenu ? `right-0`: `-right-full`} w-full md:w-auto py-8 md:p-2 bg-dark-hard text-white text-lg md:bg-inherit md:text-inherit md:static transition-all ease-in duration-500 md:transition-none`}>
-                        <nav className='main-nav flex flex-col md:flex-row flex-wrap gap-8 items-center justify-center'>
+                    <div className={`nav-wrapper absolute top-[80px] ${showMobileMenu ? `right-0`: `-right-full`} w-full lg:w-auto py-8 lg:p-2 bg-dark-hard text-white text-lg lg:bg-inherit lg:text-inherit lg:static transition-all ease-in duration-500 lg:transition-none`}>
+                        <nav className='main-nav flex flex-col lg:flex-row gap-8 items-center justify-center'>
                             <div>
-                                <ul className='menus flex flex-col md:flex-row gap-8 font-semibold'>
+                                <ul className='menus flex flex-col lg:flex-row gap-8 font-semibold'>
                                     {menus.map((item)=> <Menu menuitem={item} />)}
                                 </ul>
                             </div>
